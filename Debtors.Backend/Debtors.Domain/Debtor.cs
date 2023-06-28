65a3c125a4439f358852e03a88f13b82b47f1e7a
Aleksandr <voitenko97@yandex.ru>
2023-06-28 21:15:05 +0300

    Basic achritecture
    Составлена базовая архитектура проекта

---
+++ Debtors.Backend/Debtors.Domain/Debtor.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Debtors.Domain
+{
+    internal class Debtor
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string DebtorName { get; set; }
+        public string DebtorObject { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set;}
+    }
+}
