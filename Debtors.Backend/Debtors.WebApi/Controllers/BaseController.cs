0328790efecf98737bde3c9f69a54fd28ba82aa0
voitenko97 <voitenko97@yandex.ru>
2023-06-29 15:54:40 +0300

    Auto stash before merge of "Develop" and "DependencyInjection"

---
+++ Debtors.Backend/Debtors.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Debtors.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class BaseController : ControllerBase
+    {
+    }
+}
